f772f6b4df2e11cf8f05d65292fb70edde85de0e
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-10-31 16:18:00 -0400

    Added new objects for tweet images

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -18,31 +18,32 @@ angular
           name: "CodeToJoy",
           handle: "CodeToJoy1",
           tweet: "R2D51-52 <a href=\"https://twitter.com/hashtag/100DaysOfCode?src=hash\">#100DaysOfCode<\/a> SQL JOIN, More Java. Didn’t get the job I interviewed for. It was a good xp tho! Hoping I get another opportunity.",
-          image: null,
       },{
           photo: "https://pbs.twimg.com/profile_images/744777232002875392/NEUCj20S_bigger.jpg",
           name: "100daysofCode",
           handle: "_100DaysOfCode",
           tweet: "Hi <a href=\"https://twitter.com/rmahesh\">@rmahesh<\/a> thanks for the follow! What are you working on today?",
-          image: null,
       },{
           photo: "https://pbs.twimg.com/profile_images/717789136606928896/ddrecp7p_bigger.jpg",
           name: "edX",
           handle: "edXOnline",
           tweet: "Want to learn how to get people to engage with your brand, but not sure where to begin? Start here: <a href=\"https://t.co/CebhzqCIxm\">http://bit.ly/2lissBf<\/a>",
-          image: null,
+          image: {
+            url: "https://pbs.twimg.com/card_img/923369945496514560/b41bYPkO?format=jpg&name=386x202",
+            heading: "Digital Branding and Engagement",
+            description: "Learn how to increase brand engagement through the creation and distribution of content using an owned digital channel approach.",
+            link: "edx.org",
+          }
       },{
           photo: "https://pbs.twimg.com/profile_images/744777232002875392/NEUCj20S_bigger.jpg",
           name: "100daysofCode",
           handle: "_100DaysOfCode",
           tweet: "Thanks for the following <a href=\"https://twitter.com/Tru2Lane\">@Tru2Lane<\/a>! How are you today?",
-          image: null,
       },{
           photo: "https://pbs.twimg.com/profile_images/744777232002875392/NEUCj20S_bigger.jpg",
           name: "100daysofCode",
           handle: "_100DaysOfCode",
           tweet: "Thanks for the following <a href=\"https://twitter.com/johndv217\">@johndv217<\/a>! How are you today?",
-          image: null,
       },{
           photo: "https://pbs.twimg.com/profile_images/915295723813453824/tBI_Cb2R_bigger.jpg",
           name: "Gautam Saxena",
@@ -54,13 +55,20 @@ angular
           name: "freeCodeCamp",
           handle: "freeCodeCamp",
           tweet: "Your ultimate guide to the Google Maps SDK on iOS using Swift 4, by <a href=\"https://twitter.com/atanasov_d\">@atanasov_d<\/a>",
-          image: null,          
+          image: {
+            url: "https://pbs.twimg.com/card_img/922906080891604992/lDukXER8?format=jpg&name=600x314",
+            heading: "Your ultimate guide to the Google Maps SDK on iOS, using Swift 4",
+            description: "Many iOS apps use Google Maps. This is a very common feature, so I have decided to prepare an ultimate guide on the Google Maps SDK for iOS...",
+            link: "medium.freecodecamp.org",
+          },          
       },{
           photo: "https://pbs.twimg.com/profile_images/546011298627334146/MAeF2-yK_bigger.jpeg",
           name: "CSS-Tricks",
           handle: "Real_CSS_Tricks",
           tweet: "One thing CSS grid allows you to do is _name things_, so when you go to place things, it's clear what's going on.<a href=\"https://t.co/PNO6vV3CAZ\">https://www.smashingmagazine.com/2017/10/naming-things-css-grid-layout/<\/a>",
-          image: null,          
+          image: {
+            url: "https://pbs.twimg.com/media/DNEZ6FQV4AAKPqn.jpg",
+          },          
       },{
           photo: "https://pbs.twimg.com/profile_images/902543400901132292/m_G290UH_bigger.jpg",
           name: "Hitesh Joshi",

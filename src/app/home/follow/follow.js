f2d4f89e2faab27ef1ba90c5d7a7b2e8f1f03cbc
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-11-01 17:00:19 -0400

    Created follow component

---
+++ src/app/home/follow/follow.js
@@ -0,0 +1,24 @@
+angular
+  .module('app')
+  .component('follow', {
+    templateUrl: 'app/home/follow/follow.html',
+    controller: function() {
+      vm = this;
+      vm.follow = [
+        {
+            photo: "https://pbs.twimg.com/profile_images/696357709361188864/3Ghh7TKT_bigger.jpg",
+            name: "Francesco",
+            handle: "Fragno92",
+        },{
+            photo: "https://pbs.twimg.com/profile_images/896061559788552192/p7K_iGym_bigger.jpg",
+            name: "Treehouse",
+            handle: "Treehouse",
+        },{
+            photo: "https://pbs.twimg.com/profile_images/803647146914217984/eLpTsAdA_bigger.jpg",
+            name: "JayHouston",
+            handle: "JayHouston",
+        }
+    ]
+    }
+  });
+

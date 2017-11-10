28ccbba4081f726b668c5e3cc68095e998b7df2b
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-11-10 11:33:02 -0400

    Added interactions to first tweet to test modal

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -6,10 +6,48 @@ angular
     var vm = this;
     vm.tweets = [
         {
-            photo: "https://pbs.twimg.com/profile_images/861332770374770688/MSbiM1ec_bigger.jpg",
-            name: "CodeToJoy",
-            handle: "CodeToJoy1",
-            tweet: "R2D51-52 <a href=\"https://twitter.com/hashtag/100DaysOfCode?src=hash\">#100DaysOfCode<\/a> SQL JOIN, More Java. Didn’t get the job I interviewed for. It was a good xp tho! Hoping I get another opportunity."
+            photo: "https://pbs.twimg.com/profile_images/880376639024181248/AKw8lytD_bigger.jpg",
+            name: "Sean Doran",
+            handle: "_TheSeaning",
+            tweet: "Brand new Jupiter pics from @NASAJuno Perijove 09! What a blimmin' gorgeous/diabolical planet. Smörgåsbord: https://flic.kr/s/aHsm5Jxuv3",
+            interactions: [{
+                photo: "https://pbs.twimg.com/profile_images/560143598750924800/3n6mceDC_bigger.jpeg",
+                name: "Michael",
+                handle: "mflux",
+                time: "16h",
+                replyingto: "Replying to @_TheSeaning @NASAJuno",
+                tweet: "I would like to also see visible spectrum unmodified photo"
+            },{
+                photo: "https://pbs.twimg.com/profile_images/880376639024181248/AKw8lytD_bigger.jpg",
+                name: "Sean Doran",
+                handle: "_TheSeaning",
+                time: "16h",
+                tweet: "All photos are modified in some way.",
+                image: {
+                    url: "https://pbs.twimg.com/card_img/928031530110406656/3pPeguYF?format=jpg&name=600x314",
+                    heading: "South Tropical Zone - Juno",
+                    description: "JunoCam image of the South Tropical Zone. This image captures numerous small (20-50km) convective systems rising above the surrounding clouds...",
+                    link: "https://t.co/qC4RTBcxXn"
+                }
+            },{
+                photo: "https://pbs.twimg.com/profile_images/560143598750924800/3n6mceDC_bigger.jpeg",
+                name: "Michael",
+                handle: "mflux",
+                time: "17h",
+                tweet: "Yeah, and they look great! STILL curious though, what it would look like without modification you know? I know it won't be as spectacular but can't blame me for being curious, just feels more honest."
+            },{
+                photo: "https://pbs.twimg.com/profile_images/880376639024181248/AKw8lytD_bigger.jpg",
+                name: "Sean Doran",
+                handle: "_TheSeaning",
+                time: "17h",
+                tweet: "No worries, the colors in the images I post are there in real life, they are just enhanced to show detail. Here is another example of natural color",
+            },{
+                photo: "https://pbs.twimg.com/profile_images/560143598750924800/3n6mceDC_bigger.jpeg",
+                name: "Michael",
+                handle: "mflux",
+                time: "17h",
+                tweet: "Really gorgeous still. Our eyes are very limited!"
+              }]
         },{
             photo: "https://pbs.twimg.com/profile_images/744777232002875392/NEUCj20S_bigger.jpg",
             name: "100daysofCode",

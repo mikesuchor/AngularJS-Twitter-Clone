f48c62a8ac3889393cbd615b9576f8bfcccfa480
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-11-29 10:08:31 -0400

    Added postTweetInteraction function and changed onPostTweet to new onUpdateTweet function

--- src/app/common/composeTweet/composeTweet.js
+++ src/app/common/composeTweet/composeTweet.js
@@ -3,19 +3,33 @@ angular
 .component('composeTweet', {
   templateUrl: 'app/common/composeTweet/composeTweet.html',
   bindings: {
+    tweet: '<',
     type: '@',
-    onPostTweet: '&'
+    onUpdateTweet: '&'
   },
   controller: function(TweetService) {
     var vm = this;
     vm.postTweet = function(composetweet) {
       TweetService.postTweet(composetweet).$promise.then(function(success) {
-          vm.onPostTweet();
+          vm.onUpdateTweet();
       });
     }
-    vm.postTweetInteraction = function(composetweet) {
-      TweetService.postTweetInteraction(composetweet).$promise.then(function(success) {
-        vm.onPostTweet();
+    vm.postTweetInteraction = function(tweet, text) {
+      var composetweet = {
+        photo: "https://pbs.twimg.com/profile_images/821536751642673153/JlEInrNR_bigger.jpg",
+        name: "Michael Suchorolski",
+        handle: "mikesuchor",
+        time: "testing",
+        replyingto: "testing",
+        tweet: text,
+        date: new Date().toString()
+      };
+      TweetService.postTweetInteraction(tweet, composetweet).$promise.then(function(success) {
+        if (tweet.interactions === null || tweet.interactions === undefined) {
+          tweet.interactions = [];
+        }
+        tweet.interactions.push(composetweet);
+        vm.onUpdateTweet();
       });
     }
   }

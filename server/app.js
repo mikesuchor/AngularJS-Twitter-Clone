397fc4ca23ab576e5eb31019d992283a9b1c2905
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-12-05 09:32:04 -0400

    Added routes to tweets file

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var hello = require('./routes/hello');
+var tweets = require('./routes/tweets');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/hello', hello);
+app.use('/tweets', tweets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

bd264ff738e1858475c3e1cc5948e77de1894bbf
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-12-20 12:34:52 -0400

    Refactored postTweet function to include a promise

--- src/app/services/tweet/tweet.service.js
+++ src/app/services/tweet/tweet.service.js
@@ -2,7 +2,7 @@ angular
 .module('app')
 .service('TweetService', TweetService);
 
-function TweetService($resource) {
+function TweetService($resource, $q) {
   var vm = this;
   var expressTweetResource = $resource('http://localhost:5000/tweets/:id/:interactionId', {id: '@id', interactionId: '@interactionId'});
 
@@ -17,13 +17,17 @@ function TweetService($resource) {
   }
   
   vm.postTweet = function(composetweet) {
-    return expressTweetResource.save(
+    var deferred = $q.defer();
+    expressTweetResource.save(
       {
         photo: "https://pbs.twimg.com/profile_images/821536751642673153/JlEInrNR_bigger.jpg",
         name: "Michael Suchorolski",
         handle: "mikesuchor",
         tweet: composetweet
-      });
+      }).$promise.then(function(success){
+        deferred.resolve(success);
+      })
+    return deferred.promise;
   }
 
   vm.deleteTweet = function(id) {

a8a9adf91b5b075c79ca0c714c0eb7bd4048940a
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-12-22 16:23:56 -0400

    Added unit tests

--- src/app/common/tweet/tweet.spec.js
+++ src/app/common/tweet/tweet.spec.js
@@ -1,45 +1,63 @@
-// describe('tweet component', function () {
-//   beforeEach(module('app', function ($provide) {
-//     $provide.factory('tweet', function () {
-//       return {
-//         templateUrl: 'app/tweet.html'
-//       };
-//     });
-//   }));
-
-//   it('should...', angular.mock.inject(function ($rootScope, $compile) {
-//     var element = $compile('<tweet></tweet>')($rootScope);
-//     $rootScope.$digest();
-//     expect(element).not.toBeNull();
-//   }));
-// });
-
 describe('tweet component', function () {
-  var $componentController;
+  var $componentController, $q, $scope;
+  var deferred, promise;
+  var parent, id;
   
   beforeEach(module('app'));
-  beforeEach(inject(function(_$componentController_) {
+  beforeEach(inject(function(_$componentController_, _TweetService_, _$uibModal_, _$q_, _$rootScope_) {
     $componentController = _$componentController_;
+    TweetService = _TweetService_;
+    $uibModal = _$uibModal_;
+    $q = _$q_;
+    $scope = _$rootScope_.$new();
+    deferred = $q.defer();
+    promise = deferred.promise;
+    parent = {interactions: [{id: 1}]};
+    id = 1;
   }));
 
   it('should open a tweet detail modal', function() {
-    var ctrl = $componentController('tweet', null);
-    var openTweetSpy = spyOn(ctrl, 'open');
+    var ctrl = $componentController('tweet', $uibModal);
+    spyOn($uibModal, 'open');
     ctrl.open();
-    expect(openTweetSpy).toHaveBeenCalled();
+    expect($uibModal.open).toHaveBeenCalled();
   });
 
-  it('should delete a tweet', function() {
-    var ctrl = $componentController('tweet', null);
-    var deleteTweetSpy = spyOn(ctrl, 'deleteTweet');
-    ctrl.deleteTweet();
-    expect(deleteTweetSpy).toHaveBeenCalled();
+  it('should call TweetService after deleting a tweet', function() {
+    spyOn(TweetService, 'deleteTweet').and.returnValue(promise);
+    var ctrl = $componentController('tweet', TweetService);
+    ctrl.deleteTweet(id);
+    expect(TweetService.deleteTweet).toHaveBeenCalledWith(id);
   });
 
-  it('should delete a tweet interaction', function() {
-    var ctrl = $componentController('tweet', null);
-    var deleteTweetInteractionSpy = spyOn(ctrl, 'deleteTweetInteraction');
-    ctrl.deleteTweetInteraction();
-    expect(deleteTweetInteractionSpy).toHaveBeenCalled();
+  it('should call TweetService after deleting a tweet interaction', function() {
+    spyOn(TweetService, 'deleteTweetInteraction').and.returnValue(promise);
+    var ctrl = $componentController('tweet', TweetService);
+    ctrl.deleteTweetInteraction(parent, id);
+    expect(TweetService.deleteTweetInteraction).toHaveBeenCalledWith(parent, id);
+  });
+
+  /* Requires ES6 for findIndex method */
+  it('should splice an interaction by id after resolving deleteTweetInteraction', function() {
+    var onUpdateTweetSpy = jasmine.createSpy('onUpdateTweet');
+    var bindings = { onUpdateTweet: onUpdateTweetSpy };
+    spyOn(TweetService, 'deleteTweetInteraction').and.returnValue(promise);
+    var ctrl = $componentController('tweet', TweetService, bindings);
+    ctrl.deleteTweetInteraction(parent, id);
+    deferred.resolve();
+    $scope.$apply();
+    expect(parent.interactions.length).toEqual(0);
+  });
+
+  /* Requires ES6 for findIndex method */
+  it('should run onUpdate() after resolving deleteTweetInteraction', function() {
+    var onUpdateTweetSpy = jasmine.createSpy('onUpdateTweet');
+    var bindings = {onUpdateTweet: onUpdateTweetSpy};
+    spyOn(TweetService, 'deleteTweetInteraction').and.returnValue(promise);
+    var ctrl = $componentController('tweet', TweetService, bindings);
+    ctrl.deleteTweetInteraction(parent, id);
+    deferred.resolve();
+    $scope.$apply();
+    expect(onUpdateTweetSpy).toHaveBeenCalled();
   });
 });

a54412b3ed1087689cf7809a31a5c1e9d2344fe0
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-12-22 16:23:05 -0400

    Temporarily replaced PhantomJS browser with Chrome for ES6 support

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
       outputDir: 'test-reports'
     },
     browsers: [
-      'PhantomJS'
+      'Chrome'
     ],
     frameworks: [
       'phantomjs-shim',
@@ -37,6 +37,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-junit-reporter'),
       require('karma-coverage'),
+      require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-phantomjs-shim'),
       require('karma-ng-html2js-preprocessor'),

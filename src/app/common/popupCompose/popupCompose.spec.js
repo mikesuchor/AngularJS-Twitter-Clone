8a2b85f4c528ce4633bab285be66defdc20c7d19
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-12-26 11:56:05 -0400

    Added unit test

--- src/app/common/popupCompose/popupCompose.spec.js
+++ src/app/common/popupCompose/popupCompose.spec.js
@@ -1,12 +1,13 @@
 describe('popupCompose component', function () {
-    var $componentController, $q;
+    var $componentController, $q, $scope;
     var deferred, promise;
     
     beforeEach(module('app'));
-    beforeEach(inject(function(_$componentController_, _TweetService_, _$q_) {
+    beforeEach(inject(function(_$componentController_, _TweetService_, _$q_, _$rootScope_) {
       $componentController = _$componentController_;
       TweetService = _TweetService_;
       $q = _$q_;
+      $scope = _$rootScope_.$new();
       deferred = $q.defer();
       promise = deferred.promise;
     }));
@@ -17,5 +18,21 @@ describe('popupCompose component', function () {
       var composeTweet = { tweet: "test" };
       ctrl.postTweet(composeTweet);
       expect(TweetService.postTweet).toHaveBeenCalledWith(composeTweet);
+    });
+
+    it('should call modalInstance.close after posting a tweet', function() {
+      var onCloseSpy = jasmine.createSpy('onClose');
+      var bindings = {
+        modalInstance: {
+          close: onCloseSpy
+        }
+      };
+      spyOn(TweetService, 'postTweet').and.returnValue(promise);
+      var ctrl = $componentController('popupCompose', TweetService, bindings);
+      var composeTweet = { tweet: "test" };
+      ctrl.postTweet(composeTweet);
+      deferred.resolve();
+      $scope.$apply();
+      expect(onCloseSpy).toHaveBeenCalled();
     });
   });

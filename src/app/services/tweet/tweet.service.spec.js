9dd09192678a0d40ca12f6d94333424f45737d64
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2017-12-28 15:51:30 -0400

    Added unit tests

--- src/app/services/tweet/tweet.service.spec.js
+++ src/app/services/tweet/tweet.service.spec.js
@@ -1,135 +1,83 @@
 describe('TweetService service', function () {
-  var $http, $httpBackend;
-  var expressTweetResource = 'http://localhost:5000/tweets/';
+  var $httpBackend, $q;
+  var id = 1;
+  var interactionId = 1;
+  var tweet = {id: 1};
+  var parent = {id: 1};
+  var composetweet = "testcomposetweet";
+  var expressTweetResource = 'http://localhost:5000/tweets';
+  var expressTweetResourceWithId = 'http://localhost:5000/tweets/' + id;
 
   beforeEach(module('app'));
-  beforeEach(inject(function(_$http_, _$httpBackend_, _TweetService_) {
-    $http = _$http_;
+  beforeEach(function() {
+    installPromiseMatchers();
+  });
+  beforeEach(inject(function(_$httpBackend_, _TweetService_, _$q_) {
     $httpBackend = _$httpBackend_;
     TweetService = _TweetService_;
+    $q = _$q_;
+    deferred = $q.defer();
+    promise = deferred.promise;
   }));
 
   describe('vm.getTweets', function () {
     it('should return an array containing all tweets and reverse the array', function() {
-      var $scope = {};
-
+      deferred.resolve();
       $httpBackend.expectGET(expressTweetResource).respond([{id: 1}, {id: 2}, {id: 3}]);
-
-      $http.get(expressTweetResource).then(function(result) {
-        $scope.response = result.data;
-      });
-
+      var result = TweetService.getTweets();
       $httpBackend.flush();
-
-      expect($scope.response.reverse()).toEqual([{id: 3}, {id: 2}, {id: 1}]);
+      expect(result).toBeResolvedWith([{id: 3}, {id: 2}, {id: 1}]);
     });
   });
 
-  /* 
-  / Other way to do test above 
-  */
-  
-  // describe('vm.getTweets', function () {
-  //   it('should return an array containing all tweets and reverse the array', function() {
-  //     var $scope = {};
-
-  //     $http.get(expressTweetResource)
-  //     .then(function(data, status, headers, config) {
-  //       $scope.tweets = data;
-  //     });
-
-  //     $httpBackend
-  //     .when('GET', expressTweetResource)
-  //     .respond([{id: 1}, {id: 2}, {id: 3}]);
-
-  //     $httpBackend.flush();
-
-  //     expect($scope.tweets.data.reverse()).toEqual([{id: 3}, {id: 2}, {id: 1}]);
-  //   });
-  // });
-
   describe('vm.getTweetById', function () {
-    it('should return a tweet by its id', function() {
-      var $scope = {};
-
-      $http.get(expressTweetResource + '1')
-      .then(function(data, status, headers, config) {
-        $scope.tweet = data;
-      });
-
-      $httpBackend
-      .when('GET', expressTweetResource + '1')
-      .respond({id: 1});
-
+    it('should return an object by its id', function() {
+      $httpBackend.expectGET(expressTweetResourceWithId).respond({photo:":)", name:"A", handle:"@abc", id:id});
+      var result = TweetService.getTweetById(id);
       $httpBackend.flush();
-
-      expect($scope.tweet.data).toEqual({id: 1});
+      expect(result).toBeResolvedWith({photo:":)", name:"A", handle:"@abc", id:id});
     });
   });
 
   describe('vm.postTweet', function () {
-    it('should return an array containing all tweets with the new tweet appended', function() {
-      var $scope = {};
-
-      $http.post(expressTweetResource, {id: 4})
-      .then(function(data, status, headers, config) {
-        $scope.tweet = data;
-      });
-
-      $httpBackend
-      .when('POST', expressTweetResource, {id: 4})
-      .respond([{id: 1}, {id: 2}, {id: 3}, {id: 4}]);
-
+    it('should post a tweet', function() {
+      $httpBackend.expectPOST(expressTweetResource).respond({photo: "https://pbs.twimg.com/profile_images/821536751642673153/JlEInrNR_bigger.jpg",
+      name: "Michael Suchorolski",
+      handle: "mikesuchor",
+      tweet: composetweet});
+      var result = TweetService.postTweet(composetweet);
       $httpBackend.flush();
-      expect($scope.tweet.data).toEqual([{id: 1}, {id: 2}, {id: 3}, {id: 4}]);
+      expect(result).toBeResolvedWith({photo: "https://pbs.twimg.com/profile_images/821536751642673153/JlEInrNR_bigger.jpg",
+      name: "Michael Suchorolski",
+      handle: "mikesuchor",
+      tweet: composetweet});
     });
   });
 
   describe('vm.deleteTweet', function () {
-    it('should delete a tweet using its id and return the array containing all tweets', function() {
-      var $scope = {};
-
-      $httpBackend.expectDELETE(expressTweetResource + '1').respond([{id: 2}, {id: 3}]);
-
-      $http.delete(expressTweetResource + '1').then(function(result) {
-        $scope.response = result.data;
-      });
-
+    it('should delete a tweet using its id', function() {
+      $httpBackend.expectDELETE(expressTweetResourceWithId).respond({photo:":)", name:"A", handle:"@abc", id:id});
+      var result = TweetService.deleteTweet(id);
       $httpBackend.flush();
-
-      expect($scope.response).toEqual([{id: 2}, {id: 3}]);
+      expect(result).toBeResolvedWith({photo:":)", name:"A", handle:"@abc", id:id});
     });
   });
 
   describe('vm.postTweetInteraction', function () {
     it('should post a tweet interaction to a tweet', function() {
-      var $scope = {};
-
-      $httpBackend.expectPOST(expressTweetResource + '1').respond([{id: 1, interactions: [{id: 1}]}]);
-
-      $http.post(expressTweetResource + '1').then(function(result) {
-        $scope.response = result.data;
-      });
-
+      $httpBackend.expectPOST(expressTweetResourceWithId).respond({id: tweet.id, tweet: composetweet});
+      var result = TweetService.postTweetInteraction(tweet, composetweet);
       $httpBackend.flush();
-
-      expect($scope.response).toEqual([{id: 1, interactions: [{id: 1}]}]);
+      expect(result).toBeResolvedWith({id: tweet.id, tweet: composetweet});
     });
   });
 
   describe('vm.deleteTweetInteraction', function () {
     it('should delete a tweet interaction', function() {
-      var $scope = {};
-
-      $httpBackend.expectDELETE(expressTweetResource + '1' + '/2').respond([{id: 1, interactions: [{id: 1}]}]);
-
-      $http.delete(expressTweetResource + '1' + '/2').then(function(result) {
-        $scope.response = result.data;
-      });
-
+      $httpBackend.expectDELETE(expressTweetResourceWithId + '/' + interactionId).respond({id: parent.id, interactionId: id});
+      var result = TweetService.deleteTweetInteraction(parent, id);
       $httpBackend.flush();
-
-      expect($scope.response).toEqual([{id: 1, interactions: [{id: 1}]}]);
+      expect(result).toBeResolvedWith({id: parent.id, interactionId: id});
     });
   });
 });

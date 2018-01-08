2dfead316425a3690a004741e71c558849d765be
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-08 18:23:51 -0400

    Updated Delete Interaction e2e test

--- e2e/deleteinteraction.spec.js
+++ e2e/deleteinteraction.spec.js
@@ -1,31 +1,22 @@
 describe('deleting a tweet interaction', function() {
 
     beforeEach(function() {
-        /* 
-        / Bug - Not refreshing the page after posting a tweet 
-        */
         browser.get('http://localhost:3000/');
         var randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        element(by.model('composetweet')).sendKeys(randomString);
-        element(by.css('.compose-tweet-btn')).click();
-        element(by.model('composetweet')).sendKeys(' ');
+        element.all(by.model('composetweet')).first().sendKeys(randomString);
+        element.all(by.css('.compose-tweet-btn')).first().click();
+        element.all(by.css('.tweets-section')).first().click();
+        element.all(by.model('composetweet')).first().sendKeys(randomString);
+        element.all(by.css('.compose-tweet-btn')).first().click();
     });
 
     it('should delete a tweet interaction', function() {
-        browser.get('http://localhost:3000/');
-        var interactionCount = element.all(by.repeater("tweet in $ctrl.tweets")).count();
+        var interactionCount = element.all(by.repeater("interaction in $ctrl.tweet.interactions")).count();
         interactionCount.then(function(data) {
-            console.log(interactionCount);
-            console.log(data);
-            element.all(by.css('.click-container')).first().click();
             element.all(by.css('.interaction-dropdown')).first().click();
             element.all(by.css('[ng-click="$ctrl.deleteTweetInteraction($ctrl.parent, $ctrl.tweet.id)"]')).first().click();
-            var interactionCountAfterDelete = element.all(by.repeater("tweet in $ctrl.tweets")).count();
+            var interactionCountAfterDelete = element.all(by.repeater("interaction in $ctrl.tweet.interactions")).count();
             expect(interactionCountAfterDelete).toEqual(data-1);
         });
-    });
-
-    // Delete created tweet
-    afterEach(function() {
     });
 });

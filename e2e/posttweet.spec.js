e41d41b2a36408e7d9f5915ac501179e94e8104b
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-08 18:06:37 -0400

    Updated Post Tweet e2e test

--- e2e/posttweet.spec.js
+++ e2e/posttweet.spec.js
@@ -1,13 +1,9 @@
 describe('writing a tweet', function() {
     it('should post a tweet', function() {
-        /* 
-        / Bug - Not refreshing the page after posting a tweet 
-        */
         browser.get('http://localhost:3000/');
         var randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         element(by.model('composetweet')).sendKeys(randomString);
         element(by.css('.compose-tweet-btn')).click();
-        element(by.model('composetweet')).sendKeys(' ');
         expect(element.all(by.binding('$ctrl.tweet.tweet')).first().getText()).toEqual(randomString);
     });
 });

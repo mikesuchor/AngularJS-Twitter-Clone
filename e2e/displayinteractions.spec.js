bb640edfcf02f1c611808437399ccac95634f8c1
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-08 18:14:06 -0400

    Updated Display Interactions e2e test

--- e2e/displayinteractions.spec.js
+++ e2e/displayinteractions.spec.js
@@ -1,12 +1,17 @@
 describe('clicking on a tweet', function() {
-    it('should open the tweet and display interactions', function() {
+
+    beforeEach(function() {
         browser.get('http://localhost:3000/');
-
+        var randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        element.all(by.model('composetweet')).first().sendKeys(randomString);
+        element.all(by.css('.compose-tweet-btn')).first().click();
         element.all(by.css('.tweets-section')).first().click();
-        
-        var interactions = element.all(by.repeater('interaction in $ctrl.tweet.interactions')).count();
+        element.all(by.model('composetweet')).first().sendKeys(randomString);
+        element.all(by.css('.compose-tweet-btn')).first().click();
+    });
 
-        // Use a tweet with an interaction
-        expect(interactions).toEqual(jasmine.any(Number));
+    it('should open the tweet and display interactions', function() {
+        var interactions = element.all(by.repeater('interaction in $ctrl.tweet.interactions')).count();
+        expect(interactions).toEqual(1);
     });
 });

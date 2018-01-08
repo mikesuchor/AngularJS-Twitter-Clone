ad72a0ef7f9c3c2c38bf42392e5dfeeaceaed7ff
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-08 18:07:36 -0400

    Updated Display Tweets e2e test

--- e2e/displaytweets.spec.js
+++ e2e/displaytweets.spec.js
@@ -1,9 +1,7 @@
 describe('main page', function() {
     it('should display tweets', function() {
         browser.get('http://localhost:3000/');
-
         var elements = element.all(by.repeater('tweet in $ctrl.tweets')).count();
-        
         expect(elements).not.toBeLessThan(1);
     });
 });

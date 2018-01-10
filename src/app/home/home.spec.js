d414c1dd578c0023991def722fea2337942c6fe6
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-10 17:16:28 -0400

    Fixed spacing for consistency

--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -1,27 +1,27 @@
 describe('home component', function () {
-    var $componentController, $q, $scope;
-    var deferred, promise;
+  var $componentController, $q, $scope;
+  var deferred, promise;
     
-    beforeEach(module('app'));
-    beforeEach(inject(function(_$componentController_, _TweetService_, _$q_, _$rootScope_) {
-      $componentController = _$componentController_;
-      TweetService = _TweetService_;
-      $q = _$q_;
-      $scope = _$rootScope_.$new();
-      deferred = $q.defer();
-      promise = deferred.promise;
-    }));
+  beforeEach(module('app'));
+  beforeEach(inject(function(_$componentController_, _TweetService_, _$q_, _$rootScope_) {
+    $componentController = _$componentController_;
+    TweetService = _TweetService_;
+    $q = _$q_;
+    $scope = _$rootScope_.$new();
+    deferred = $q.defer();
+    promise = deferred.promise;
+  }));
   
-    it('should call TweetService to get tweets', function() {
-        spyOn(TweetService, 'getTweets').and.returnValue(promise);
-        TweetService.getTweets();
-        expect(TweetService.getTweets).toHaveBeenCalled();
-    });
+  it('should call TweetService to get tweets', function() {
+    spyOn(TweetService, 'getTweets').and.returnValue(promise);
+    TweetService.getTweets();
+    expect(TweetService.getTweets).toHaveBeenCalled();
+  });
 
-    it('should call TweetService to get tweets after updating a tweet', function() {
-        spyOn(TweetService, 'getTweets').and.returnValue(promise);
-        var ctrl = $componentController('home', TweetService);
-        ctrl.onUpdateTweet();
-        expect(TweetService.getTweets).toHaveBeenCalled();
-    });
+  it('should call TweetService to get tweets after updating a tweet', function() {
+    spyOn(TweetService, 'getTweets').and.returnValue(promise);
+    var ctrl = $componentController('home', TweetService);
+    ctrl.onUpdateTweet();
+    expect(TweetService.getTweets).toHaveBeenCalled();
+  });
 });

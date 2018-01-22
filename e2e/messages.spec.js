ac09ab78d6c9a80612c70e34f8829f36baac2b72
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-22 18:00:36 -0400

    Added E2E tests for messages

--- e2e/messages.spec.js
+++ e2e/messages.spec.js
@@ -3,7 +3,7 @@ var TwitterMainPage = require('./twittermainpage.po.js');
 describe('Twitter App', function() {
   var twitterMainPage = new TwitterMainPage();
 
-  it('should navigate to messages endpoint', function() {
+  it('should navigate to messages endpoint from navbar', function() {
     twitterMainPage.loadHomePage();
     twitterMainPage.clickNavbarMessagesLink();
     expect(browser.getCurrentUrl()).toBe('http://localhost:3000/messages');

4ed7f75b8e3dc45d96a9bef765c922c973908f98
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-23 18:31:28 -0400

    Added new unit test to check if messages are being loaded

--- e2e/messages.spec.js
+++ e2e/messages.spec.js
@@ -3,9 +3,15 @@ var TwitterMainPage = require('./twittermainpage.po.js');
 describe('Twitter App', function() {
   var twitterMainPage = new TwitterMainPage();
 
-  it('should navigate to messages endpoint from navbar', function() {
+  it('should navigate to /messages from navbar', function() {
     twitterMainPage.loadHomePage();
     twitterMainPage.clickNavbarMessagesLink();
     expect(browser.getCurrentUrl()).toBe('http://localhost:3000/messages');
+  });
+
+  it('should contain 3 messages in /messages', function() {
+    twitterMainPage.loadHomePage();
+    twitterMainPage.clickNavbarMessagesLink();
+    expect(twitterMainPage.messagesCount()).toBeGreaterThan(0);
   });
 });

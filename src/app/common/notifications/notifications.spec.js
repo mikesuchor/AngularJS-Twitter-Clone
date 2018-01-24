2a2a493b1dd9ba52fe326571b260b9d46054d3cb
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-24 17:00:06 -0400

    Fixed linebreak-style using windows line endings

--- src/app/common/notifications/notifications.spec.js
+++ src/app/common/notifications/notifications.spec.js
@@ -1,15 +1,53 @@
-describe('notifications component', function() {
-  beforeEach(module('app', function($provide) {
-    $provide.factory('notifications', function() {
-      return {
-        templateUrl: 'app/notifications.html'
-      };
-    });
-  }));
+describe('login route', function() {
+    'use strict';
 
-  it('should...', angular.mock.inject(function($rootScope, $compile) {
-    var element = $compile('<notifications></notifications>')($rootScope);
-    $rootScope.$digest();
-    expect(element).not.toBeNull();
-  }));
+    var $httpBackend, $q, $state, $templateCache, $location;
+
+    var tweetServiceMock = jasmine.createSpyObj('TweetService', [
+        'getTweets'
+    ]);
+
+    function mockServices($provide) {
+        $provide.factory('TweetService', function() {
+            return tweetServiceMock;
+        });
+    }
+
+    function services($injector) {
+        $q = $injector.get('$q');
+        $state = $injector.get('$state');
+        $httpBackend = $injector.get('$httpBackend');
+        $templateCache = $injector.get('$templateCache');
+        $location = $injector.get('$location');
+    }
+
+    function setUp() {
+        $httpBackend.whenGET(/assets\/(.+)/).respond(200, {});
+    }
+
+    beforeEach(function() {
+        module('app', mockServices);
+        inject(services);
+        setUp();
+    });
+
+    describe('when navigating to `/`', function() {
+        function goTo(url) {
+            $location.url(url);
+            $httpBackend.flush();
+        }
+
+        beforeEach(function() {
+            tweetServiceMock.getTweets.and.returnValue($q.resolve([]));
+        });
+
+        describe('and user is logged in', function() {
+            it('redirects to the home state', function() {
+                $location.url('/notifications');
+
+                expect(tweetServiceMock.getTweets).toHaveBeenCalled();
+                expect($state.current.name).toBe('notifications');
+            });
+        });
+    });
 });

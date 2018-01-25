15fc68539a68ef0827b0fe63fc504753a4297853
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-25 16:36:59 -0400

    Added notification.spec update, not working currently

--- src/app/common/notifications/notifications.spec.js
+++ src/app/common/notifications/notifications.spec.js
@@ -1,53 +1,35 @@
-describe('login route', function() {
-    'use strict';
+describe('notifications component', function() {
+  var $q;
+  var $state;
+  var $httpBackend;
+  var $location;
 
-    var $httpBackend, $q, $state, $templateCache, $location;
+  var tweetServiceMock = jasmine.createSpyObj('TweetService', ['getTweets']);
 
-    var tweetServiceMock = jasmine.createSpyObj('TweetService', [
-        'getTweets'
-    ]);
-
-    function mockServices($provide) {
-        $provide.factory('TweetService', function() {
-            return tweetServiceMock;
-        });
-    }
-
-    function services($injector) {
-        $q = $injector.get('$q');
-        $state = $injector.get('$state');
-        $httpBackend = $injector.get('$httpBackend');
-        $templateCache = $injector.get('$templateCache');
-        $location = $injector.get('$location');
-    }
-
-    function setUp() {
-        $httpBackend.whenGET(/assets\/(.+)/).respond(200, {});
-    }
-
-    beforeEach(function() {
-        module('app', mockServices);
-        inject(services);
-        setUp();
+  function mockServices($provide) {
+    $provide.factory('TweetService', function() {
+        return tweetServiceMock;
     });
+  }
 
-    describe('when navigating to `/`', function() {
-        function goTo(url) {
-            $location.url(url);
-            $httpBackend.flush();
-        }
+  function setUp() {
+    $httpBackend.whenGET(/assets\/(.+)/).respond(200, {});
+  }
 
-        beforeEach(function() {
-            tweetServiceMock.getTweets.and.returnValue($q.resolve([]));
-        });
+  beforeEach(module('app', mockServices));
+  beforeEach(inject(function(_$q_, _$state_, _$httpBackend_, _$location_) {
+    $q = _$q_;
+    $state = _$state_;
+    $httpBackend = _$httpBackend_;
+    $location = _$location_;
+    setUp();
+  }));
 
-        describe('and user is logged in', function() {
-            it('redirects to the home state', function() {
-                $location.url('/notifications');
+  it('should get tweets when navigating to /notifications', function() {
+    tweetServiceMock.getTweets.and.returnValue($q.resolve([]));
+    $location.url('/notifications');
 
-                expect(tweetServiceMock.getTweets).toHaveBeenCalled();
-                expect($state.current.name).toBe('notifications');
-            });
-        });
-    });
+    expect(tweetServiceMock.getTweets).toHaveBeenCalled();
+    expect($state.current.name).toBe('/notifications');
+  });
 });

a23b8a5035c3f70bb01d2d561f7eaaeec7df6f0e
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-26 17:39:29 -0400

    Added cgBusyDefaults

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 angular
-  .module('app', ['cgBusy', 'ngResource', 'ui.bootstrap', 'ui.router']);
+  .module('app', ['cgBusy', 'ngResource', 'ui.bootstrap', 'ui.router'])
+  .value('cgBusyDefaults',{
+    message:'Please Wait...',
+    backdrop: true,
+    minDuration: 1000,
+    wrapperClass: 'cg-busy-fullscreen'
+  });

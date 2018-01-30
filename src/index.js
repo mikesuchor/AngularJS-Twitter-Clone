3e6e50fd71d8a8c749ca333a33c0837bbae5bc5e
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-01-30 15:43:40 -0400

    Eslint linting fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 angular
   .module('app', ['cgBusy', 'ngResource', 'ui.bootstrap', 'ui.router'])
-  .value('cgBusyDefaults',{
-    message:'Please Wait...',
+  .value('cgBusyDefaults', {
+    message: 'Please Wait...',
     backdrop: true,
     minDuration: 3000,
     wrapperClass: 'cg-busy-fullscreen'

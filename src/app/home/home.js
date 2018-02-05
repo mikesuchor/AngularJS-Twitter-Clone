4ac858136f9c4fbe8bce1dff8d720fa8a6cb415e
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-05 16:05:07 -0400

    Added $on to receive 'NavbarUpdateTweet' event which causes a page refresh through getTweets()

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -11,10 +11,16 @@
     controller: HomeController
   });
 
-  function HomeController(TweetService) {
+  function HomeController(TweetService, $scope) {
     var vm = this;
 
     vm.onUpdateTweet = onUpdateTweet;
+
+    $scope.$on('NavbarUpdateTweet', function onUpdateTweet() {
+      vm.promise = TweetService.getTweets().then(function(success) {
+        vm.tweets = success;
+      });
+    });
 
     vm.$onInit = function() {
       vm.promise = TweetService.getTweets().then(function(success) {

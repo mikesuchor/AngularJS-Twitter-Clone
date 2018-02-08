59e92a4063695ee57153c6c3aa35b416cfbc71fe
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-08 12:25:30 -0400

    Create login component and associated html/css

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,7 +1,13 @@
-angular
-  .module('app')
-  .component('login', {
-    templateUrl: 'app/login/login.html',
-    controller: loginController
-  });
+(function() {
+  'use strict';
 
+  angular
+    .module('app')
+    .component('login', {
+      templateUrl: 'app/login/login.html',
+      controller: LoginController
+    });
+
+  function LoginController() {
+  }
+})();

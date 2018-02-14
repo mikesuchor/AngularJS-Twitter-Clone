94725196a60e84078eb068e74247501d807630d7
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-14 12:31:24 -0400

    Change redirect when visiting / to login page

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ angular
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
-  $urlRouterProvider.when('/', '/home');
+  $urlRouterProvider.when('/', '/login');
 
   $stateProvider
     .state('main', {

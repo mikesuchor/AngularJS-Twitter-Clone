1b80668a36cd797155b3f8d8b0bbcbda2a673822
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-14 12:32:12 -0400

    Add login function which redirects to main page on successful login

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -8,6 +8,15 @@
       controller: LoginController
     });
 
-  function LoginController() {
+  function LoginController(LoginService, $state) {
+    var vm = this;
+
+    vm.login = login;
+
+    function login(username, password) {
+      vm.promise = LoginService.postUser(username, password).then(function(success) {
+          $state.go('main');
+        })
+    }
   }
 })();

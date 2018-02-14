38c65a1c59c91bc85a8c87ace51d53b17afd5b00
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-14 12:31:06 -0400

    Add users route and users json

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var cors = require('cors');
+var fs = require('fs');
 var router = express.Router();
+router.options('*', cors())
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var usersJSON;
+var usersInDatabase;
+var pathToDatabase = '../server/public/users.json';
+
+fs.readFile(pathToDatabase, function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  usersJSON = JSON.parse(data);
+  usersInDatabase = usersJSON.users;
+});
+
+/* GET users listing */
+router.get('/', cors(), function(req, res, next) {
+  res.json(usersInDatabase);
+});
+
+/* POST new user to users.json file */
+router.post('/', cors(), function(req, res, next) {
+  usersInDatabase.forEach(function(dbuser) {
+    if (dbuser.username === req.body.username && dbuser.password === req.body.password) {
+      res.status(200).json();
+    }
+  });
+  res.status(400).send('Username/password combination was not found');
 });
 
 module.exports = router;

fca8a549c09b2ee7b27b5cfea13f37d5c3cebd9d
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-14 12:32:24 -0400

    Add login unit tests

--- src/app/login/login.spec.js
+++ src/app/login/login.spec.js
@@ -1,13 +1,13 @@
-describe('login component', function () {
-  beforeEach(module('app', function ($provide) {
-    $provide.factory('login', function () {
+describe('login component', function() {
+  beforeEach(module('app', function($provide) {
+    $provide.factory('login', function() {
       return {
         templateUrl: 'app/login.html'
       };
     });
   }));
 
-  it('should...', angular.mock.inject(function ($rootScope, $compile) {
+  it('should...', angular.mock.inject(function($rootScope, $compile) {
     var element = $compile('<login></login>')($rootScope);
     $rootScope.$digest();
     expect(element).not.toBeNull();

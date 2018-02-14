daeecf9a72abf1ef161e186845cd73022b6abf5a
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-14 12:32:36 -0400

    Add login service

---
+++ src/app/services/login/login.service.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+  
+    angular
+    .module('app')
+    .service('LoginService', LoginService);
+  
+    function LoginService($resource, $q) {
+      var vm = this;
+      var expressUsersResource = $resource('http://localhost:5000/users/');
+  
+      vm.postUser = postUser;
+  
+      function postUser(username, password) {
+        var deferred = $q.defer();
+        expressUsersResource.save(
+          {
+            username: username,
+            password: password
+          }
+        ).$promise.then(function(success) {
+          deferred.resolve(success);
+        });
+        return deferred.promise;
+      }
+    }
+  })();
+  

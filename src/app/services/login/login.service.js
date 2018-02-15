6635e9c338f2c6f5a5b9f2f6a3bedd74fe94d7b1
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-15 10:01:47 -0400

    Add promise resolution for failed login

--- src/app/services/login/login.service.js
+++ src/app/services/login/login.service.js
@@ -1,16 +1,16 @@
 (function() {
     'use strict';
-  
+
     angular
     .module('app')
     .service('LoginService', LoginService);
-  
+
     function LoginService($resource, $q) {
       var vm = this;
       var expressUsersResource = $resource('http://localhost:5000/users/');
-  
+
       vm.postUser = postUser;
-  
+
       function postUser(username, password) {
         var deferred = $q.defer();
         expressUsersResource.save(
@@ -18,11 +18,12 @@
             username: username,
             password: password
           }
-        ).$promise.then(function(success) {
+        ).$promise.then(function(success, failure) {
           deferred.resolve(success);
+        }, function(error) {
+          deferred.reject(error);
         });
         return deferred.promise;
       }
     }
   })();
-  

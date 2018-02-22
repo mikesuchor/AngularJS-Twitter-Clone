c50ee4f36d9b3936722229cfcd2163be762376a8
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-02-22 16:57:45 -0400

    Add ocLazyLoad to different states (not currently working)

--- src/routes.js
+++ src/routes.js
@@ -14,22 +14,47 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
     })
     .state('login', {
       url: '/login',
-      component: 'login'
+      component: 'login',
+      resolve: {
+        loadComponent: function($ocLazyLoad) {
+          return $ocLazyLoad.load('app/login/login.module.js');
+        }
+      }
     })
     .state('main.home', {
       url: 'home',
-      component: 'home'
+      component: 'home',
+      resolve: {
+        loadComponent: function($ocLazyLoad) {
+          return $ocLazyLoad.load('app/home/home.module.js');
+        }
+      }
     })
     .state('main.notifications', {
       url: 'notifications',
-      component: 'notifications'
+      component: 'notifications',
+      resolve: {
+        loadComponent: function($ocLazyLoad) {
+          return $ocLazyLoad.load('app/common/notifications/notifications.module.js');
+        }
+      }
     })
     .state('main.mentions', {
       url: 'mentions',
-      component: 'mentions'
+      component: 'mentions',
+      resolve: {
+        loadComponent: function($ocLazyLoad) {
+          return $ocLazyLoad.load('app/common/mentions/mentions.module.js');
+        }
+      }
     })
     .state('main.messages', {
       url: 'messages',
-      component: 'messages'
+      component: 'messages',
+      resolve: {
+        loadComponent: function($ocLazyLoad) {
+          return $ocLazyLoad.load('app/common/messages/messages.module.js');
+        }
+      }
     });
 }
